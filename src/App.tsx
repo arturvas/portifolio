81f1fec1ff65adb68a42ba30e2e44bd29c02c35a
Artur Vasconcelos <artur.vasconcelos8@gmail.com>
2024-03-24 09:46:28 -0400

    feat: implement `container`, `sidebar`, `about`, `project`, components.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-import GlobalStyle from './styles'
+import About from './containers/About'
+import Projects from './containers/Projects'
+import Sidebar from './containers/Sidebar'
+import GlobalStyle, { Container } from './styles'
 
 function App() {
   return (
     <>
       <GlobalStyle />
+      <Container>
+        <Sidebar />
+        <main>
+          <About />
+          <Projects />
+        </main>
+      </Container>
     </>
   )
 }
