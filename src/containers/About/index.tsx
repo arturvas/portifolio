2e5eab2935f580e42d467d2ac746fabb76407f9e
Artur Vasconcelos <artur.vasconcelos8@gmail.com>
2024-03-25 11:08:22 -0400

    feat: add github section

--- src/containers/About/index.tsx
+++ src/containers/About/index.tsx
@@ -1,5 +1,6 @@
 import Title from '../../components/Title'
 import Paragraph from '../../components/Paragraph'
+import { GithubSection } from './styles'
 
 const About = () => (
   <section>
@@ -10,6 +11,10 @@ const About = () => (
       soluta dolor vitae optio maxime itaque debitis! Itaque iusto quia non
       saepe dolor!
     </Paragraph>
+    <GithubSection>
+      <img src="https://github-readme-stats.vercel.app/api?username=arturvas&show_icons=true&theme=dracula&include_all_commits=true&count_private=true" />
+      <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=arturvas&layout=compact&langs_count=7&theme=dracula" />
+    </GithubSection>
   </section>
 )
 
