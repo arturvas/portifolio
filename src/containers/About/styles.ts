5f64c6936125059d14cdaaccc045eeea65f3e075
Artur Vasconcelos <artur.vasconcelos8@gmail.com>
2024-04-11 15:14:18 -0400

    style: Add responsive layout for GitHub section

--- src/containers/About/styles.ts
+++ src/containers/About/styles.ts
@@ -3,8 +3,14 @@ import styled from 'styled-components'
 export const GithubSection = styled.div`
   margin-top: 32px;
   margin-bottom: 64px;
+  display: flex;
+  justify-content: space-between;
 
   img {
     height: 165px;
+  }
+
+  @media (max-width: 768px) {
+    display: block;
   }
 `
