41e267302883e0e44e23d5682444c91d56213947
Artur Vasconcelos <artur.vasconcelos8@gmail.com>
2024-04-12 13:16:51 -0400

    feat: add light theme props

---
+++ src/themes/light.ts
@@ -0,0 +1,8 @@
+const lightTheme = {
+  mainColor: '#282a35',
+  secondaryColor: '#949494',
+  backgroundColor: '#fff',
+  backgroundButtonColor: '#4476bf',
+  borderColor: '#c1c1c1'
+}
+export default lightTheme
