5f5209007dc592c9d794f27ed0064145806397b6
Artur Vasconcelos <artur.vasconcelos8@gmail.com>
2024-04-12 13:16:57 -0400

    feat: add dark theme props

---
+++ src/themes/dark.ts
@@ -0,0 +1,8 @@
+const darkTheme = {
+  mainColor: '#fff',
+  secondaryColor: '#eee',
+  backgroundColor: '#282a35',
+  backgroundButtonColor: '#fff',
+  borderColor: '#c1c1c1'
+}
+export default darkTheme
