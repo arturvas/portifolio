4552e241e7b58632e7189864d655a6a3b1eb2485
Artur Vasconcelos <artur.vasconcelos8@gmail.com>
2024-04-12 13:19:48 -0400

    feat: add change theme button and setting props

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,31 @@
+import { ThemeProvider } from 'styled-components'
+import { useState } from 'react'
+
 import About from './containers/About'
 import Projects from './containers/Projects'
 import Sidebar from './containers/Sidebar'
 import GlobalStyle, { Container } from './styles'
+import darkTheme from './themes/dark'
+import lightTheme from './themes/light'
 
 function App() {
+  const [areUsingDarkTheme, setAreUsingDarkTheme] = useState(true)
+
+  function changeTheme() {
+    setAreUsingDarkTheme(!areUsingDarkTheme)
+  }
+
   return (
-    <>
+    <ThemeProvider theme={areUsingDarkTheme ? darkTheme : lightTheme}>
       <GlobalStyle />
       <Container>
-        <Sidebar />
+        <Sidebar changeTheme={changeTheme} />
         <main>
           <About />
           <Projects />
         </main>
       </Container>
-    </>
+    </ThemeProvider>
   )
 }
 
